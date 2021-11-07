c0a4baade53020aa5d32bda30f8346880deab8d7
Brian Wagner <bhw2113@columbia.edu>
2021-11-07 18:44:27 -0500

    Improve performance with libscrypt (#1)

--- Package.swift
+++ Package.swift
@@ -12,26 +12,23 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(name: "web3swift", targets: ["web3swift"]),
     ],
+    
     dependencies: [
-        .package(url: "https://github.com/attaswift/BigInt.git", from: "5.3.0"),
-        .package(url: "https://github.com/mxcl/PromiseKit.git", from: "6.15.4"),
+        .package(url: "https://github.com/attaswift/BigInt.git", from: "5.2.0"),
+        .package(url: "https://github.com/mxcl/PromiseKit.git", from: "6.15.3"),
         .package(url: "https://github.com/daltoniam/Starscream.git", from: "4.0.4"),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.4.2"),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.4.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "secp256k1",
-            path: "Sources/secp256k1"),
+        .target(name: "secp256k1"),
+        .target(name: "libscrypt"),
         .target(
             name: "web3swift",
-            dependencies: ["BigInt", "secp256k1", "PromiseKit", "Starscream", "CryptoSwift"],
-            path: "Sources/web3swift"),
+            dependencies: ["BigInt", "secp256k1", "PromiseKit", "Starscream", "CryptoSwift", "libscrypt"]),
         .testTarget(
             name: "web3swiftTests",
-            dependencies: ["web3swift"],
-            path: "Tests/web3swiftTests"),
-    ],
-    swiftLanguageVersions: [.v5]
+            dependencies: ["web3swift"]),
+    ]
 )
